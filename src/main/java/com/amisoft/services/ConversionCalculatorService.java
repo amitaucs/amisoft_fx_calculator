a3214ce1d08afb8f48e01b7a82c90cfa2b68e734
amitdatta <the.amistudy@gmail.com>
2017-08-06 03:46:18 +0530

    Regular expression corrected

--- src/main/java/com/amisoft/services/ConversionCalculatorService.java
+++ src/main/java/com/amisoft/services/ConversionCalculatorService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 
 @Service
 public class ConversionCalculatorService {
